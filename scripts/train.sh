30fd2a8ffb430fc348fe0f5b6d6d7fae5b819cc6
XDong18 <liuxd1215@gmail.com>
2021-01-05 04:30:01 +0800

    cc

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7,8
-PORT=29503 ./tools/dist_train.sh configs/yolact/yolact_r101_8x1_multi_size_bdd100k.py 8 --work-dir out/yolact_r101_8x1_multi_size_bdd100k
+PORT=29503 ./tools/dist_train.sh configs/yolact/yolact_r101_8x1_multi_size_bdd100k.py 4 --work-dir out/yolact_r101_8x1_multi_size_bdd100k
